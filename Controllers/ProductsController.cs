06666c2a0197aca2b945ca31dda2ad8e28fd59c1
Domino <ritokral2008@mail.ru>
2026-07-06 08:56:27 +0800

    Add ProductsController

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,75 @@
+﻿using InternetShop.DTOs;
+using InternetShop.Models;
+using InternetShop.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace InternetShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController: ControllerBase
+    {
+        private readonly IProductService<Product> _service;
+
+        public ProductsController(
+            IProductService<Product> service)
+        {
+            _service = service;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_service.GetAll());
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok(_service.GetById(id));
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public IActionResult Post(ProductDto dto)
+        {
+            var product = new Product
+            {
+                Name = dto.Name,
+                Price = dto.Price,
+                Stock = dto.Stock
+            };
+
+            _service.Add(product);
+
+            return Created("", product);
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, ProductDto dto)
+        {
+            var product = new Product
+            {
+                Name = dto.Name,
+                Price = dto.Price,
+                Stock = dto.Stock
+            };
+
+            _service.Update(id, product);
+
+            return Ok();
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _service.Remove(id);
+
+            return NoContent();
+        }
+    }
+}
